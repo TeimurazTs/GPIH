e57d282e05cd3ca4c88e6e82a1d984a908b02501
TeimurazTs <temotsuladze99@gmail.com>
2023-11-16 03:05:44 +0400

    add final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared/button/button.component';
 import { InputComponent } from './shared/input/input.component';
-import { RegisterComponent } from "./pages/register/register.component";
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, ButtonComponent, InputComponent, RegisterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonComponent,
+    InputComponent,
+    RegisterComponent,
+    RouterModule,
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GPI';
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {
+    this.route.navigate(['/']);
+  }
 }
